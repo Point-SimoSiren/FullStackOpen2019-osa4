78682ad1a28c63c8356b6387899c8540998c7085
Simo Siren <simo.siren@stud.pointcollege.fi>
2019-10-19 08:34:31 +0300

    3 unit  tests passed inc favoriteBlog

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,25 @@
-const dummy = (blogs) => {
+const dummy = array => {
     return 1
 }
 
+const totalLikes = array => {
+    const Likes = (array.map(a => a.likes))
+    const reducer = (accumulator, currentValue) => accumulator + currentValue
+
+    return (Likes.reduce(reducer))
+}
+
+const favoriteBlog = array => {
+    const Likes = (array.map(a => a.likes))
+    const greatest = Math.max(...Likes)
+
+    for (i = 0; i < array.length; i++) {
+        if (array[i].likes === greatest) {
+            return (({ author, likes, title }) => ({ author, likes, title }))(array[i])
+        }
+    }
+}
+
 module.exports = {
-    dummy
+    dummy, totalLikes, favoriteBlog
 }
