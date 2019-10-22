9244ebc74f6080a358416bd6aeccb9d308300c56
Simo Siren <simo.siren@stud.pointcollege.fi>
2019-10-23 00:21:55 +0300

    user schema done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
+const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
@@ -18,11 +19,13 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
         logger.error('Error. Couldn´t connect MongoDB:', error.message)
     })
 
+
 app.use(cors())
 app.use(express.static('build'))
 app.use(bodyParser.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.unknownEndpoint)
